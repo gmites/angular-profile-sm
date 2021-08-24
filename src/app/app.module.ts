aa52cf14c389ea71f74158c6d34c1ced3cead024
DESKTOP-HFE9SM7\khaleesi <grace.mites@gmail.com>
2021-08-24 13:47:20 +0200

    new empty component profile-picture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ProfileInfoComponent } from './profile-info/profile-info.component';
+import { ProfilePictureComponent } from './profile-picture/profile-picture.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ProfileInfoComponent
+    ProfileInfoComponent,
+    ProfilePictureComponent
   ],
   imports: [
     BrowserModule
