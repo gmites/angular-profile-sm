fea50c0d1867e69ddc6ac9a092b35e8df2e606e4
DESKTOP-HFE9SM7\khaleesi <grace.mites@gmail.com>
2021-08-24 13:46:40 +0200

    New component profile-info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ProfileInfoComponent } from './profile-info/profile-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProfileInfoComponent
   ],
   imports: [
     BrowserModule
