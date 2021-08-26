963fc4426a1cf756cca6a560740140f36243b5de
DESKTOP-HFE9SM7\khaleesi <grace.mites@gmail.com>
2021-08-26 12:02:14 +0200

    Map with current location

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, NgZone } from '@angular/core';
+import { MapsAPILoader } from '@agm/core';
 
 @Component({
   selector: 'app-map',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
+  latitude: number= 35.6894;
+  longitude: number= 139.692;
+  zoom: number=15;
 
-  constructor() { }
+  @ViewChild('search')
+  public searchElementRef!: ElementRef;
 
-  ngOnInit(): void {
+  constructor(
+    private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone
+  ) { }
+
+  ngOnInit() {
+    this.mapsAPILoader.load().then(() => {
+    });
   }
 
+  getCurrentLocation(){
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+      });
+    }
+  }
 }
+
